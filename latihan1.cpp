f89a46252b66a18aa324ad2f4823de17a219310e
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-12-09 20:55:30 +0700

    ubah ubah

--- latihan1.cpp
+++ latihan1.cpp
@@ -5,41 +5,24 @@ using namespace std;
 int interatif (int x){
 int n, c, f1 = 0, f2 = 1, next;
 
-cout <<"Masukan Nilai =";cin>>n;cout<<endl;
-cout <<"Nilai =" <<n<< " fibonacci:-" <<endl;
-for (c=0;c < n ; c++ )
-
-    {
-
+cout <<"Masukkan banyak bilangan dalam deret fibonnaci Anda: ";cin>>n;cout<<endl;
+cout <<"Banyak bilangan = " <<n<< " fibonacci:-" <<endl;
+for (c=0;c < n ; c++ ){
       if (c<=1)
-
          next = c;
-
-      else
-
-      {
-
+      else {
          next = f1 + f2;
-
          f1 = f2;
-
          f2 = next;
+         }
 
+      cout<<next<<endl;
       }
-
-         cout<<next<<endl;
-
-      }
-
 }
 
 int main() {
-
    int x;
-
    interatif(x);
 
-
 return 0;
-
 }
--- latihan3.cpp
+++ latihan3.cpp
@@ -1,17 +1,34 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
 
 using namespace std;
-void balik(char *s)
-{ if (*s != '\0'){
-balik(&s[1]);
-cout << s[0];
-}
-}
-int main()
-{
-      char* kata = (char*) "iis";
-    balik(kata); cout << endl;
-    return 0;
 
-}
+ int kali1(int a, int b){
+    int hasil =0;
+    for(int i=0;i<b;i++){
+        hasil=hasil+a;
+    }
+    return hasil;
+ }
+
+ int kali2(int a, int b){
+    if(b==0)
+        return 1;
+    else if(b==1)
+            return a;
+        else
+            return a+kali2(a,b-1);
+ }
+
+ int main(int argc, char *argv[]){
+    int a,b;
+    cout<<"masukkan a :";
+    cin>>a;
+    cout<<"masukkan b :";
+    cin>>b;
+    cout<<"secara ITERATIF :"<<endl;
+    cout<<kali1(a,b)<<endl;
+    cout<<"secara REKURSIF"<<endl;
+    cout<<kali2(a,b)<<endl;
+
+ return 0;
+ }
